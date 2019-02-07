f47a7de8e5762316335fd4ae10467f2f83b0ded8
Dip Dhanesha <dhanesha@Dips-MBP.frontierlocal.net>
2019-02-06 23:19:45 -0500

    Time fighter game.

---
+++ app/src/main/java/com/example/testapplication/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.example.testapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    internal lateinit var tapMeButton: Button
+    internal lateinit var gameScoreTextView: TextView
+    internal lateinit var timeLeftTextView: TextView
+    internal var score = 0
+    internal var gameStarted = false
+    internal lateinit var countDownTimer: CountDownTimer
+    internal val initialCountDown: Long = 60000
+    internal val countDownInterval: Long = 1000
+
+    private fun startGame() {
+        countDownTimer.start()
+        gameStarted = true
+    }
+    private fun incrementScore() {
+        if (!gameStarted){
+            startGame()
+        }
+        score += 1
+        val newScore = getString(R.string.your_score, score.toString())
+        gameScoreTextView.text = newScore
+    }
+
+    private fun endGame() {
+        Toast.makeText(this,getString(R.string.game_over, score.toString()), Toast.LENGTH_LONG).show()
+        resetGame()
+    }
+
+    private fun resetGame() {
+        score = 0
+        gameScoreTextView.text = getString(R.string.your_score, score.toString())
+        val initialTimeLeft = initialCountDown / 1000
+        timeLeftTextView.text = getString(R.string.time_left, initialTimeLeft.toString())
+
+        countDownTimer = object: CountDownTimer(initialCountDown, countDownInterval) {
+            override fun onTick(miliSecondUntilFinished: Long) {
+                val timeLeft = miliSecondUntilFinished / 1000
+                timeLeftTextView.text = getString(R.string.time_left, timeLeft.toString())
+            }
+
+            override fun onFinish() {
+                endGame()
+            }
+        }
+        gameStarted = false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        tapMeButton = findViewById<Button>(R.id.tap_button)
+        gameScoreTextView = findViewById<TextView>(R.id.game_score_text)
+        timeLeftTextView = findViewById<TextView>(R.id.time_left_text_view)
+        resetGame()
+
+        tapMeButton.setOnClickListener { view ->
+            incrementScore()
+        }
+    }
+}
